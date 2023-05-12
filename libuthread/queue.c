83333403a553a6abc67c41e5e302ad203d374043
Nicz76 <niczop2167@gmail.com>
2023-05-11 18:11:11 -0700

    Add code

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -4,70 +4,71 @@
 
 #include "queue.h"
 
+
 struct queue {
 	int size;
-	struct queue *head;
-	struct queue *tail;
-	struct queue *prev;
-	struct queue *next;
-	void * data;
+	struct queue* head;
+	struct queue* tail;
+	struct queue* prev;
+	struct queue* next;
+	void* data;
 
 };
 
 queue_t queue_create(void)
 {
 	queue_t queue;
-	if (NULL != (queue = malloc(sizeof(struct queue)))){
-		if (queue == NULL){
-		return NULL;
+	if (NULL != (queue = malloc(sizeof(struct queue)))) {
+		if (&queue == NULL) {
+			return NULL;
 		}
 		queue->size = 0;
 		queue->head = NULL;
 		queue->tail = NULL;
 	}
-		return queue;
+	return queue;
 }
 
 int queue_destroy(queue_t queue)
 {
-	if (queue == NULL || !queue->size != 0){
+	if (queue == NULL || queue->size != 0) {
 		return -1;
 	}
-	
-	else{
-		while (!queue_empty(queue)){
-			free(queue_top(queue));
-			queue_dequeue(queue);
+
+	else {
+		while (!(!queue->size)) {
+			free(queue->head->data);
+			queue_dequeue(queue, &(queue->data));
 		}
-	return 0;
+		return 0;
 	}
 }
 
-int queue_enqueue(queue_t queue, void *data)
+int queue_enqueue(queue_t queue, void* data)
 {
 	// If the queue or data fails to give data, return error
-	if (queue == NULL || data == NULL){
+	if (queue == NULL || data == NULL) {
 		return -1;
 	}
 
 	queue_t Newqueue;
-	Newqueue = malloc(sizeof(struct qitem));
+	Newqueue = malloc(sizeof(struct queue));
 
-	if (Newqueue == NULL){
-	 	return -1;
+	if (Newqueue == NULL) {
+		return -1;
 	}
 
 	Newqueue->data = data;
 	Newqueue->next = NULL;
 
 	//If the enqueue is the first data
-	if (queue_empty(queue)){
+	if (queue->size == 0) {
 		queue->head = Newqueue;
 		queue->tail = Newqueue;
 		Newqueue->prev = NULL;
 		Newqueue->next = NULL;
 	}
-	else{
+	else {
 		queue->tail->next = Newqueue;
 		Newqueue->prev = queue->tail;
 		Newqueue->next = NULL;
@@ -78,41 +79,55 @@ int queue_enqueue(queue_t queue, void *data)
 	return 0;
 }
 
-int queue_dequeue(queue_t queue, void **data)
+int queue_dequeue(queue_t queue, void** data)
 {
-	if (queue == NULL || data == NULL || queue_empty(queue)){
+	if (queue == NULL || data == NULL || queue->size <= 0) {
 		return -1;
 	}
 
 	queue_t item;
 
-	if (!queue_empty(queue)){
+	if (queue->size > 0) {
 		item = queue->head;
-		data = queue->head->data;
+		data = &(queue->head->data);
 		queue->head = item->next;
 
-		if (queue->head){
+		if (queue->head) {
 			queue->head->prev = NULL;
 		}
-		else{
+		else {
 			queue->tail = NULL;
 		}
 		--queue->size;
 		free(item);
 	}
+	return 0;
 }
 
-int queue_delete(queue_t queue, void *data)
+int queue_delete(queue_t queue, void* data)
 {
-	if (queue == NULL || data==NULL || queue_empty(queue)){
+	if (queue == NULL || data == NULL) {
 		return -1;
 	}
-		queue_t item;
+	queue_t item;
+	item = queue;
 
-	while (!queue_empty(queue))
+	while ((queue->size) > 0)
 	{
-		if (data == queue->)
+		if (data == item->data) {
+			item->prev->next = item->next;
+			item->next = NULL;
+			item->prev = NULL;
+			free(item);
+			return 0;
+		}
+		
+		item->prev = item;
+		item = item->next;
+		--queue->size;
 	}
+
+	return -1;
 	/* TODO Phase 1 */
 	/*
  * queue_delete - Delete data item
@@ -125,62 +140,35 @@ int queue_delete(queue_t queue, void *data)
  * Return: -1 if @queue or @data are NULL, of if @data was not found in the
  * queue. 0 if @data was found and deleted from @queue.
  */
-
 }
+
+
 
 int queue_iterate(queue_t queue, queue_func_t func)
 {
-	/* Callback function that increments items */
-static void iterator_inc(queue_t q, void *data)
-{
-    int *a = (int*)data;
+	if (queue == NULL || func == NULL) {
+		return -1;
+	}
+	int count = 0;
+	queue_t item;
+	item = queue;
 
-    if (*a == 42)
-        queue_delete(q, data);
-    else
-        *a += 1;
-}
-
-void test_iterator(void)
-{
-    queue_t q;
-    int data[] = {1, 2, 3, 4, 5, 42, 6, 7, 8, 9};
-    size_t i;
-
-    /* Initialize the queue and enqueue items */
-    q = queue_create();
-    for (i = 0; i < sizeof(data) / sizeof(data[0]); i++)
-        queue_enqueue(q, &data[i]);
-
-    /* Increment every item of the queue, delete item '42' */
-    queue_iterate(q, iterator_inc);
-    assert(data[0] == 2);
-    assert(queue_length(q) == 9);
-}
-	/* TODO Phase 1 */
-/*
- * queue_iterate - Iterate through a queue
- * @queue: Queue to iterate through
- * @func: Function to call on each queue item
- *
- * This function iterates through the items in the queue @queue, from the oldest
- * item to the newest item, and calls the given callback function @func on each
- * item. The callback function receives the current data item as parameter.
- *
- * Note that this function should be resistant to data items being deleted
- * as part of the iteration (ie in @func).
- *
- * Return: -1 if @queue or @func are NULL, 0 otherwise.
- */
+	while ((item->size) != count)
+	{
+	func(item, item->data);
+	item->prev = item;
+	item = item->next;
+	count++;
+	}
+	
 }
 
 int queue_length(queue_t queue)
 {
-	if (queue == NULL){
+	if (queue == NULL) {
 		return -1;
 	}
-	else{
-		return !queue->size;
+	else {
+		return queue->size;
 	}
 }
-
